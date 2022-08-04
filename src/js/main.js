4f4318af3e3876908653dbbb3ccebf31ae6f864a
Maksim Ivanovskiy <ivanovskiy.maxim@mail.ru>
2022-08-04 16:57:00 -0400

    renamed function arguments, now plural

--- src/js/main.js
+++ src/js/main.js
@@ -6,16 +6,16 @@ window.addEventListener('DOMContentLoaded', () => {
     modals();
 
     tabs({
-        headerSelector: '.glazing_slider',
-        tabSelector: '.glazing_block',
-        contentSelector: '.glazing_content',
+        headersSelector: '.glazing_slider',
+        tabsSelector: '.glazing_block',
+        contentItemsSelector: '.glazing_content',
         activeClass: '.active'
     });
 
     tabs({
-        headerSelector: '.decoration_slider',
-        tabSelector: '.no_click',
-        contentSelector: '.decoration_content > div > div',
+        headersSelector: '.decoration_slider',
+        tabsSelector: '.no_click',
+        contentItemsSelector: '.decoration_content > div > div',
         activeClass: 'after_click'
     });
 });
--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,7 +1,7 @@
-export const tabs = ({headerSelector, tabSelector, contentSelector, activeClass}) => {
-    const header = document.querySelector(headerSelector),
-          tab = document.querySelectorAll(tabSelector),
-          content = document.querySelectorAll(contentSelector);
+export const tabs = ({headersSelector, tabsSelector, contentItemsSelector, activeClass}) => {
+    const header = document.querySelector(headersSelector),
+          tab = document.querySelectorAll(tabsSelector),
+          content = document.querySelectorAll(contentItemsSelector);
     
     function hideTabContent() {
         content.forEach(item => {
@@ -24,8 +24,8 @@ export const tabs = ({headerSelector, tabSelector, contentSelector, activeClass}
     header.addEventListener('click', (e) => {        //навешиваем обработчик события на обшую площадь которая соединяет в себе все табы
         const target = e.target;
         if (target &&
-            (target.classList.contains(tabSelector.replace(/\./, "")) ||         //проверяем, что мы действительно кликнули на один из табов
-        target.parentNode.classList.contains(tabSelector.replace(/\./, "")))) {  
+            (target.classList.contains(tabsSelector.replace(/\./, "")) ||         //проверяем, что мы действительно кликнули на один из табов
+        target.parentNode.classList.contains(tabsSelector.replace(/\./, "")))) {  
             tab.forEach((item, i) => {                                    //перебираем эти табы и запоминает не только таб но и номер по порядку(i)
                 if (target == item || target.parentNode == item) {      //как только пользователь кликнул на таб который мы перебираем, мы используем его индекс(i)
                     hideTabContent();
