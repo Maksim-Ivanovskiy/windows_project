2c049d02b5b0d1ae28402e88ead15389c5206949
Maksim Ivanovskiy <ivanovskiy.maxim@mail.ru>
2022-08-04 17:00:43 -0400

    renamed variables and arguments to plural form

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,6 +1,6 @@
 export const tabs = ({headersSelector, tabsSelector, contentItemsSelector, activeClass}) => {
-    const header = document.querySelector(headersSelector),
-          tab = document.querySelectorAll(tabsSelector),
+    const headers = document.querySelector(headersSelector),
+          tabs = document.querySelectorAll(tabsSelector),
           content = document.querySelectorAll(contentItemsSelector);
     
     function hideTabContent() {
@@ -8,26 +8,26 @@ export const tabs = ({headersSelector, tabsSelector, contentItemsSelector, activ
             item.style.display = 'none';
         });
 
-        tab.forEach(item => {
-            item.classList.remove(activeClass);
+        tabs.forEach(tab => {
+            tab.classList.remove(activeClass);
         });
     }
 
     function showTabContent(i = 0) {
         content[i].style.display = 'block';
-        tab[i].classList.add(activeClass);
+        tabs[i].classList.add(activeClass);
     }
 
     hideTabContent();
     showTabContent();
 
-    header.addEventListener('click', (e) => {        //навешиваем обработчик события на обшую площадь которая соединяет в себе все табы
+    headers.addEventListener('click', (e) => {        //навешиваем обработчик события на обшую площадь которая соединяет в себе все табы
         const target = e.target;
         if (target &&
             (target.classList.contains(tabsSelector.replace(/\./, "")) ||         //проверяем, что мы действительно кликнули на один из табов
         target.parentNode.classList.contains(tabsSelector.replace(/\./, "")))) {  
-            tab.forEach((item, i) => {                                    //перебираем эти табы и запоминает не только таб но и номер по порядку(i)
-                if (target == item || target.parentNode == item) {      //как только пользователь кликнул на таб который мы перебираем, мы используем его индекс(i)
+            tabs.forEach((tab, i) => {                                    //перебираем эти табы и запоминает не только таб но и номер по порядку(i)
+                if (target == tab || target.parentNode == tab) {      //как только пользователь кликнул на таб который мы перебираем, мы используем его индекс(i)
                     hideTabContent();
                     showTabContent(i);
                 }
