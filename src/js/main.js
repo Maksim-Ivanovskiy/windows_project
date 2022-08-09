338014a9732da9aae106c233603b1693e21bf839
Maksim Ivanovskiy <ivanovskiy.maxim@mail.ru>
2022-08-09 16:24:39 -0400

    fixed issues

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,13 @@
 import "./slider";
-import {modals, tabs, forms, changeModalState} from "./modules/index";
-
+import { modals, tabs, forms, changeModalState } from "./modules/index";
 
 window.addEventListener('DOMContentLoaded', () => {
     
-    let modalState = {};
+    const modalState = {};
 
     changeModalState(modalState);
     modals();
-/*  tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
-    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
-    tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
-    forms(modalState); */
-
+    
     tabs({
         headersSelector: '.glazing_slider',
         tabsSelector: '.glazing_block',
