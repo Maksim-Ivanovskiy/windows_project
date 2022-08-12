24d1a6bb16bcc1ad899d0b52689bb8957442cf02
Maksim Ivanovskiy <ivanovskiy.maxim@mail.ru>
2022-08-11 20:36:12 -0400

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,5 @@
 import "./slider";
 
-/* import modals from "./modules/modals";
-import tabs from "./modules/tabs";
-import forms from "./modules/forms";
-import changeModalState from "./modules/changeModalState";
-import timer from "./modules/timer";  */
-    
 import { modals, tabs, forms, changeModalState, timer } from "./modules/index";
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -40,5 +34,4 @@ window.addEventListener('DOMContentLoaded', () => {
 
     forms(modalState);
     timer('.container1', deadLine);
-
 });
